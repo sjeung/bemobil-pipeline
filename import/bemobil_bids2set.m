e18eaf52e927d905bf15618587574962687893e9
Sein <seinjeung@gmail.com>
2025-04-08 14:59:58 +0200

    Bugfix in tracking system name handling
    Fixes an issue where a tracking system is lost when the name contains another tracksys name as a string

--- import/bemobil_bids2set.m
+++ import/bemobil_bids2set.m
@@ -570,8 +570,15 @@ for iSub = 1:numel(subDirList)
 
             for TSi = 1:numel(trackingSystemsInSession)
 
-                dataFiles = modalityFiles(contains(modalityFiles, trackingSystemsInSession{TSi}));
-
+                
+                dataFiles = {};
+                for fileInd = 1:numel(modalityFiles)
+                    splitname = strsplit(modalityFiles{fileInd},'_'); 
+                    if any(strcmp(splitname, trackingSystemsInSession{TSi}))
+                        dataFiles{end+1} = modalityFiles{fileInd};
+                    end
+                end
+                
                 if numel(eegFiles) ~= numel(dataFiles)
                     warning(['Number of EEG files and data files of type ' bemobilModality ' do not match within session ''' config.session_names{iSes} ''''])
                 end
